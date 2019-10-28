e291f2d057650f00c96791da48bf7c78e78dd666
gunnkrub <gunnkrub2543@gmail.com>
2019-10-28 12:03:05 +0700

    add template and view of index detail regis

--- events/models.py
+++ events/models.py
@@ -6,11 +6,10 @@ from django.utils import timezone
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    event_date = models.DateField('event date')
+    start_time = models.TimeField('start time')
+    end_time = models.TimeField('end time')
     def __str__(self):
         return self.name
 
-class Date(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.date
