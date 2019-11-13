21ee379ce3dc0341cb1492fc39d44dfcd0c13c9a
gunnkrub <gunnkrub2543@gmail.com>
2019-11-13 20:03:09 +0700

    add register system add description and modified all templates

--- events/models.py
+++ events/models.py
@@ -2,24 +2,14 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    event_date = models.DateField('event date')
-    start_time = models.TimeField('start time')
-    end_time = models.TimeField('end time')
+    description = models.CharField(max_length=300,null = True)
+    event_date = models.DateField('event date',null = True)
+    start_time = models.TimeField('start time',null = True)
+    end_time = models.TimeField('end time',null = True)
+    user = models.ManyToManyField(User, blank=True)
     def __str__(self):
         return self.name
-
-##class Duration(models.Model):
-##    duration = models.ForeignKey(Event, on_delete=models.CASCADE)
-##    def __str__(self):
-##        return self.duration
-##
-##class Description(models.Model):
-##    description = models.ForeignKey(Event, on_delete=models.CASCADE)
-##    def __str__(self):
-##        return self.description
-
--- events/views.py
+++ events/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
 from .models import Event
+from django.contrib.auth.models import User
 
 class IndexView(generic.ListView):
     model = Event
@@ -14,7 +16,7 @@ class IndexView(generic.ListView):
         """
         Return all upcoming events in the future .
         """
-        return Event.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[:10]
+        return Event.objects.filter(event_date__gte=timezone.now()).order_by('event_date')[:10]
 
 class DetailView(generic.DetailView):
     model = Event
@@ -23,3 +25,14 @@ class DetailView(generic.DetailView):
 class RegisterView(generic.ListView):    
     model = Event
     template_name = 'events/regis.html'
+
+def regis(request, pk):
+    userID = request.POST.get('UserID',False)
+    user = User.objects.get(id=userID)
+
+    regis_event = Event.objects.get(pk=pk)
+
+    regis_event.user.add(user)
+    regis_event.save()
+
+    return redirect(reverse("events:register"))
