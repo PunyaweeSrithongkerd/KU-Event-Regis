4ff86839c5b43e2c3474e8bdff436ea7ca8e2e0c
Punyawee Srithonhkerd <tonkla9874@gmail.com>
2019-11-25 05:22:52 +0700

    Setup google social oath

--- KU_Event_Regis/urls.py
+++ KU_Event_Regis/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
     path('events/', include('events.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('social_django.urls', namespace='social')),
+    url('^api/vl/', include('social_django.urls', namespace='social'))
+
          ]
