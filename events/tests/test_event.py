3c81e125eb8b4945c52d8471b59ebdbbbc74b6f8
Punyawee Srithonhkerd <tonkla9874@gmail.com>
2019-11-25 07:27:56 +0700

    Add the test to project(unfinished)

---
+++ events/tests/test_event.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from events.models import Event
+# Create your tests here.
+class TestModelEvent(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        Event.objects.create(name = 'Event')
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+    
+    def test_first_name_label(self):
+        event = Event.objects.get(id=1)
+        field_label = event._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+    
+    def test_first_name_max_length(self):
+        event = Event.objects.get(id=1)
+        max_length = event._meta.get_field('name').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_str_(self):
+        event = Event.objects.get(id=1)
+        name_return = event.name()
+        self.assertEquals(name_return,'Event')
+    
+    def test_event_date_label(self):
+        event = Event.objects.get(id=1)
+        field_label = event._meta.get_field('event date').verbose_name
+        self.assertEquals(field_label, 'event date')
+    
+    def test_start_time_label(self):
+        event = Event.objects.get(id=1)
+        field_label = event._meta.get_field('start time').verbose_name
+        self.assertEquals(field_label, 'start time')
+    
+    def test_end_time_label(self):
+        event = Event.objects.get(id=1)
+        field_label = event._meta.get_field('end time').verbose_name
+        self.assertEquals(field_label, 'end time')
+
---
+++ events/tests/test_views.py
@@ -0,0 +1,96 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from events.models import Event
+
+
+def create_Event(Event_text, days):
+    """
+    Create a Event with the given `Event_text` and published the
+    given number of `days` offset to now.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Event.objects.create(name=Event_text, event_date=time)
+
+
+class EventIndexViewTests(TestCase):
+    def test_no_Events(self):
+        """
+        If no Events exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('events:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No events are available.")
+        self.assertQuerysetEqual(response.context['latest_Event_list'], [])
+
+    def test_past_Event(self):
+        """
+        Events with a pub_date in the past are displayed on the
+        index page.
+        """
+        create_Event(Event_text="Past Event.", days=-30)
+        response = self.client.get(reverse('events:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_Event_list'],
+            ['<Event: Past Event.>']
+        )
+
+    def test_future_Event(self):
+        """
+        Events with a pub_date in the future aren't displayed on
+        the index page.
+        """
+        create_Event(Event_text="Future Event.", days=30)
+        response = self.client.get(reverse('events:index'))
+        self.assertContains(response, "No events are available.")
+        self.assertQuerysetEqual(response.context['latest_Event_list'], [])
+
+    def test_future_Event_and_past_Event(self):
+        """
+        Even if both past and future Events exist, only past Events
+        are displayed.
+        """
+        create_Event(Event_text="Past Event.", days=-30)
+        create_Event(Event_text="Future Event.", days=30)
+        response = self.client.get(reverse('events:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_Event_list'],
+            ['<Event: Past Event.>']
+        )
+
+    def test_two_past_Events(self):
+        """
+        The Events index page may display multiple Events.
+        """
+        create_Event(Event_text="Past Event 1.", days=-30)
+        create_Event(Event_text="Past Event 2.", days=-5)
+        response = self.client.get(reverse('events:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_Event_list'],
+            ['<Event: Past Event 2.>', '<Event: Past Event 1.>']
+        )
+
+
+class EventDetailViewTests(TestCase):
+    def test_future_Event(self):
+        """
+        The detail view of a Event with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_Event = create_Event(Event_text='Future Event.', days=5)
+        url = reverse('events:detail', args=(future_Event.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_Event(self):
+        """
+        The detail view of a Event with a pub_date in the past
+        displays the Event's text.
+        """
+        past_Event = create_Event(Event_text='Past Event.', days=-5)
+        url = reverse('events:detail', args=(past_Event.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_Event.Event_text)
