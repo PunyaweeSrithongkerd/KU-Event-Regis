3f540b9208aff3a4c219646790a174d881bf22bf
gunnkrub <gunnkrub2543@gmail.com>
2019-11-28 21:56:58 +0700

    add First name Last name and Email field on signup.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render, redirect
 
+from accounts.forms import SignUpForm
+
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -18,21 +18,5 @@ def signup(request):
             login(request, user)
             return redirect('events:index')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
-
-
-##class SignUp(generic.CreateView):
-##    form_class = UserCreationForm
-##    success_url = reverse_lazy('login')
-##    template_name = 'signup.html'
-
-##    def form_valid(self, form):
-##    to_return = super().form_valid(form)
-##    user = authenticate(
-##        username=form.cleaned_data["username"],
-##        password=form.cleaned_data["password1"],
-##    )
-##    login(self.request, user)
-##    return to_return
-
