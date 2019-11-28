e8815b9bc31b33d6b47fc30bc5e34fbd3a64bf5f
gunnkrub <gunnkrub2543@gmail.com>
2019-11-28 22:07:38 +0700

    delete comment

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,3 @@
-##from django.urls import path
-##
-##from . import views
-##
-##
-##urlpatterns = [
-##    path('signup/', views.SignUp.as_view(), name='signup'),
-##]
 from django.conf.urls import url
 from . import views
 
