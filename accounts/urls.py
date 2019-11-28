e1b86c796060bb954694c5d3d2c1f48a0dbcdc52
gunnkrub <gunnkrub2543@gmail.com>
2019-11-28 21:33:45 +0700

    finished sign up system

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-
+##from django.urls import path
+##
+##from . import views
+##
+##
+##urlpatterns = [
+##    path('signup/', views.SignUp.as_view(), name='signup'),
+##]
+from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    url('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,27 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('events:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+##class SignUp(generic.CreateView):
+##    form_class = UserCreationForm
+##    success_url = reverse_lazy('login')
+##    template_name = 'signup.html'
 
 ##    def form_valid(self, form):
 ##    to_return = super().form_valid(form)
@@ -20,3 +35,4 @@ class SignUp(generic.CreateView):
 ##    )
 ##    login(self.request, user)
 ##    return to_return
+
