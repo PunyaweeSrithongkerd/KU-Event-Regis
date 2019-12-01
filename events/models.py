a951d88a5494e905a03d9e3207cc737668845371
gunnkrub <gunnkrub2543@gmail.com>
2019-12-01 12:34:20 +0700

    add popular event on index

--- events/models.py
+++ events/models.py
@@ -12,6 +12,7 @@ class Event(models.Model):
     event_date = models.DateField('event date',null = True)
     start_time = models.TimeField('start time',null = True)
     end_time = models.TimeField('end time',null = True)
+    max_regis = models.IntegerField('maximum participant', null = True, blank = True)
     user = models.ManyToManyField(User, blank=True)
     
     def __str__(self):
@@ -20,11 +21,15 @@ class Event(models.Model):
     def is_future(self):
         if self.event_date  > timezone.now().date():
             return True
+        else:
+            return False
         
     def is_soon_event(self):
         """Return True if registered events will begin with in 3 days"""
         if (self.event_date <= timezone.now().date() + datetime.timedelta(days=3)) and self.is_future():
             return True
+        else:
+            return False
 
     def day_to_event(self):
         return (self.event_date - timezone.now().date()).days
