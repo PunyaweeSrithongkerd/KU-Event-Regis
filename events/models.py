0d86509ca6ace021a3382b6ae64fcd20101669e9
gunnkrub <gunnkrub2543@gmail.com>
2019-12-15 21:32:34 +0700

    add more decoration and logging

--- events/models.py
+++ events/models.py
@@ -11,6 +11,7 @@ TODAY = timezone.now().date()
 class Event(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=300, null=True, blank=True)
+    image = models.ImageField(upload_to='gallery', null=True, blank=True)
     location = models.CharField(max_length=200, null=True, blank=True)
     qualification = models.CharField(max_length=300, null=True, blank=True)
     event_date = models.DateField('event date', null=True)
@@ -23,9 +24,9 @@ class Event(models.Model):
         """Return event name"""
         return self.name
 
-    def is_future(self):
-        """Return True if event is in future and return False if not"""
-        if self.event_date > TODAY:
+    def is_today_or_future(self):
+        """Return True if event is in future or today and return False if not"""
+        if self.event_date >= TODAY:
             return True
         else:
             return False
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect, render_to_response
 from django.urls import reverse, reverse_lazy
 from django.views import generic
@@ -7,20 +7,21 @@ from django.template.response import TemplateResponse
 from .models import Event
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count
+from django.db.models import Q, Count
 from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
 
 import logging
-
 
 def configure():
     """Configure loggers and log handlers"""
     # write all messages to a file.
     # For a real app, use a configurable absolute path to log file.
     filehandler = logging.FileHandler("log.log")
-    filehandler.setLevel(logging.DEBUG)
+    filehandler.setLevel(logging.INFO)
     formatter = logging.Formatter(
         '%(asctime)s %(name)s %(levelname)s: %(message)s')
     filehandler.setFormatter(formatter)
@@ -49,7 +50,10 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(IndexView, self).get_context_data(*args, **kwargs)
-        context['most_popular_event'] = Event.objects.annotate(user_count=Count("user")).order_by("-user_count")[:1]
+        context['most_popular_event'] = Event.objects.annotate(
+            user_count=Count("user")).filter(event_date__gt=timezone.now()).order_by("-user_count")[:1]
+##        context['reminder'] = user.event_set.all().filter(
+##            event_date__gte=timezone.now() + datetime.timedelta(days=3)).order_by("day_to_event")
         return context
 
 
@@ -79,7 +83,7 @@ def regis(request, pk):
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         print("Logging to ", logger)
-        logger.info(f"UserID: {user.id} register {regis_event.name} event")
+        logger.info(f"ID {user.id} {user.username} register {regis_event.name} event")
         # log_test(logger)
 
     return redirect(reverse("events:register"))
@@ -98,7 +102,7 @@ def unregis(request, pk):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     print("Logging to ", logger)
-    logger.info(f"UserID: {user.id} unregister {regis_event.name} event")
+    logger.info(f"ID {user.id} {user.username} unregister {regis_event.name} event")
 
     return redirect(reverse("events:register"))
 
@@ -122,3 +126,43 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+
+
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):    
+    configure()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    ip = request.META.get('REMOTE_ADDR')
+
+    logger.info('login user: {user} via ip: {ip}'.format(
+        user=user,
+        ip=ip
+    ))
+
+@receiver(user_logged_out)
+def user_logged_out_callback(sender, request, user, **kwargs):
+    configure()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    ip = request.META.get('REMOTE_ADDR')
+
+    logger.info('logout user: {user} via ip: {ip}'.format(
+        user=user,
+        ip=ip
+    ))
+
+@receiver(user_login_failed)
+def user_login_failed_callback(sender, request, credentials, **kwargs):
+    configure()
+    logger = logging.getLogger()
+    logger.setLevel(logging.WARNING)
+    ip = request.META.get('REMOTE_ADDR')
+    
+    logger.warning('login failed for: {credentials} via ip: {ip}'.format(
+        credentials=credentials,
+        ip=ip
+    ))
+
